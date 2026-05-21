7140827263d4e6caec9ecf5e69b647d664b10c99
piyushkoche08-hub <piyuskoche08@gmail.com>
2026-05-21 10:50:38 +0530

    update

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def serve_html(page):
     return render_template(f'{page}.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
